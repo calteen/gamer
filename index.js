2dc5b8fab76ba2e8350e967ec81d5ffa37e31e5f
jay <pjay325476@gmail.com>
2022-10-09 10:43:43 +0530

    1

--- index.js
+++ index.js
@@ -1,7 +1,53 @@
+require("dotenv").config()
 const express = require("express");
 const app = express();
 
-require("dotenv").config()
+
+
+
+
+
+
+
+app.use(express.json())
+app.use(express.static('public'))
+app.use(express.static('public'))
+app.set('view engine', 'ejs');
+
+
+
+
+
+
+
+
+
+//cors
+const cors = require("cors");
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.get("/", (req, res)=>{
 res.json({success: true     });
